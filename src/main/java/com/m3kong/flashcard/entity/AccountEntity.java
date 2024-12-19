b5d2f390192793e5f68c3ae604af40f88d8c8791
mekonger <m3kong.dev@gmail.com>
2024-12-19 22:32:20 +0700

    Setup local db + define Account entity

--- src/main/java/com/m3kong/flashcard/entity/AccountEntity.java
+++ src/main/java/com/m3kong/flashcard/entity/AccountEntity.java
@@ -1,4 +1,32 @@
 package com.m3kong.flashcard.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "account")
+@DynamicInsert
+@DynamicUpdate
 public class AccountEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(columnDefinition = "varchar(255) comment 'user name'", nullable = false)
+    private String userName;
+
+    @Column(columnDefinition = "varchar(255) comment 'account email'", nullable = false, unique = true)
+    private String email;
+
+    @Column(columnDefinition = "varchar(255) comment 'hash password'", nullable = false)
+    private String hashPassword;
+
+    @Column(columnDefinition = "varchar(255) comment 'public key of hash password'", nullable = false)
+    private String saltKey;
 }
