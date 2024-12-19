449510eab06a258a1dfd99c877dfaaf717283151
mekonger <m3kong.dev@gmail.com>
2024-12-19 20:09:56 +0700

    Init source code

---
+++ src/test/java/com/m3kong/flashcard/FlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3kong.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
